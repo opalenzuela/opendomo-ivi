823913a019000b5cc83655e6fcb301a15fe319e1
opalenzuela <opalenzuela@gmail.com>
2014-01-20 10:21:34 +0100

    Cleanup

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 VERSION=`date '+%Y%m%d'`
-ARCH=noarch
-PKG="odcgi_scada"
+PKGID="odivi"
 USR="--owner 1000 --group 1000 --same-permissions "
 EXCLUDE=" --exclude '*~' --exclude .svn "
 
 
 # packages
-mkdir -p pkg
-rm -fr pkg/*.gz
-tar cfz ./pkg/$PKG-$VERSION.od.$ARCH.tar.gz usr etc var $USR $EXCLUDE
+tar cfz $PKGID-$VERSION.tar.gz usr var $USR $EXCLUDE
 
 
