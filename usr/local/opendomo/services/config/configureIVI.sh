65d7366a65b4e3491bd89187080a2b1f00436396
opalenzuela <opalenzuela@gmail.com>
2014-09-04 11:27:26 +0200

    First test with configuration

--- usr/local/opendomo/services/config/configureIVI.sh
+++ usr/local/opendomo/services/config/configureIVI.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
-#desc:Configure IVI
-#package:odcgi_scada
+#desc:Configure map
+#package:odivi
 #type:local
 
 # This script will allow the configuration of OD's IVI
 # If the configuration directory does not exist
 
 CFGDIR="/etc/opendomo/ivi"
+CFGFILE="$CFGDIR.conf"
 
-exit 1
+if ! test -d "$CFGDIR" 
+then
+	mkdir $CFGDIR
+fi
+touch $CFGFILE
+
+# If parameters are specified, we save it
+if ! test -z "$1"
+then
+	echo "FLOORS=$1" > $CFGFILE
+fi
+
+source $CFGFILE
+
+echo "#>Configure map"
+echo "form:`basename $0`"
+echo "	floors	FLOORS	number	$FLOORS"
+for i in $FLOORS
+do
+	echo "	floor1	Floor number [$i]	file	floor$i.jpg"
+done
+echo 
