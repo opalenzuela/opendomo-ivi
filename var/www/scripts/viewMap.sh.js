a022e8e4d6e9ecefe33cc3ff17fc1f72192fe4d4
opalenzuela <opalenzuela@gmail.com>
2014-09-10 12:46:59 +0200

    using real port name

--- var/www/scripts/viewMap.sh.js
+++ var/www/scripts/viewMap.sh.js
@@ -112,7 +112,7 @@ jQuery(function($) {
 			this.title=nc;			
 			//alert(this.id);
 			$.ajax({
-				url: "/cgi-bin/od.cgi/listControlPorts.sh?port="+this.id+"&value="+nc+"&GUI=XML",
+				url: "/cgi-bin/od.cgi/listControlPorts.sh?port="+this.id.replace("_","/")+"&value="+nc+"&GUI=XML",
 				context: this
 				}).done(function() {
 					$(this).toggle("highlight",{percent:0},500 );
